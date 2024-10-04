19d47d75d7dfda73e785187de0097e5be90fab47
Igo <igo.vieira@live.com>
2024-10-04 18:16:17 -0300

    primeiro commit

---
+++ src/test/java/com/powerkr_teste/powerkr/PowerkrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powerkr_teste.powerkr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerkrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
