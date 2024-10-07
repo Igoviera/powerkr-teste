4e69f701335dc50fd5c17713c0e0da642eeab910
Igo <igo.vieira@live.com>
2024-10-06 21:49:10 -0300

    modificaçoes responseDTO, mapper, service

--- src/main/java/com/powerkr_teste/powerkr/dto/RegisterDTO.java
+++
@@ -1,6 +0,0 @@
-package com.powerkr_teste.powerkr.dto;
-
-import com.powerkr_teste.powerkr.enums.UserRole;
-
-public record RegisterDTO(String name, String email, String password, UserRole role) {
-}
